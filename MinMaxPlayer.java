ef3bb70a2894e9d1ee169396eb80f16a3a6f4e30
giannishorgos <giannis.21.osfp@gmail.com>
2020-12-20 22:11:23 +0200

    Updateds

--- MinMaxPlayer.java
+++ MinMaxPlayer.java
@@ -27,8 +27,11 @@ public class MinMaxPlayer extends Player {
     	double sum = 0;
     	switch( dice){
     	case 0: // UP
+    		if( board.tiles[currentPos].isUp() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isUp()) {
-    				sum -=2;
+    				sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -52,8 +55,11 @@ public class MinMaxPlayer extends Player {
 			}
 			break;
     	case 1: // RIGHT
+    		if( board.tiles[currentPos].isRight() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isRight()) {
-    			sum -=2;
+    			sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -77,8 +83,11 @@ public class MinMaxPlayer extends Player {
         	}
     		break;
     	case 2: //DOWN
+    		if( board.tiles[currentPos].isDown() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isDown()) {
-    			sum -=2;
+    			sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -102,8 +111,11 @@ public class MinMaxPlayer extends Player {
         	}
     		break;
     	case 3: //LEFT
+    		if( board.tiles[currentPos].isLeft() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isLeft()) {
-    			sum -= 2;
+    			sum -= 1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -135,8 +147,11 @@ public class MinMaxPlayer extends Player {
     	double sum = 0;
     	switch( dice){
     	case 0: // UP
+    		if( board.tiles[currentPos].isUp() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isUp()) {
-    				sum -=2;
+    				sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -160,8 +175,11 @@ public class MinMaxPlayer extends Player {
 			}
 			break;
     	case 1: // RIGHT
+    		if( board.tiles[currentPos].isRight() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isRight()) {
-    			sum -=2;
+    			sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -185,8 +203,11 @@ public class MinMaxPlayer extends Player {
         	}
     		break;
     	case 2: //DOWN
+    		if( board.tiles[currentPos].isDown() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isDown()) {
-    			sum -=2;
+    			sum -=1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -210,8 +231,11 @@ public class MinMaxPlayer extends Player {
         	}
     		break;
     	case 3: //LEFT
+    		if( board.tiles[currentPos].isLeft() && n==1) {
+    			return 420;
+    		}
     		if( board.tiles[currentPos].isLeft()) {
-    			sum -= 2;
+    			sum -= 1;
     			break;
     		}
     		for( int i=0; i<board.getS(); i++) {
@@ -239,26 +263,210 @@ public class MinMaxPlayer extends Player {
     	return sum + movepoints;
     }
     
-    public int chooseMinMaxMove(Node root) {
-	return 5;
+    public int chooseMinMaxMove(Node root, int currentPos, int opponentCurrentPos) {
+    	double bestMinosValue=-100.0;
+    	int bestMinosMove= -1;
+    	
+    	double bestValue=-100;
+    	int bestMove= -1;
+    	
+    	for( int i=0; i< root.children.size(); i++) {
+    		for( int j=0; j< root.children.get(i).children.size(); j++) {
+    			if( root.children.get(i).children.get(j).getNodeEvaluation() > bestMinosValue) {
+    				bestMinosValue = root.children.get(i).children.get(j).getNodeEvaluation();
+    				bestMinosMove = root.children.get(i).children.get(j).getNodeMove()[2];
+    			}
+    		}
+    		switch( bestMinosMove) {
+    		case 0:
+    			root.children.get(i).setNodeEvaluation(evaluateT( currentPos, root.children.get(i).getNodeMove()[2], opponentCurrentPos + board.getN(), 1));
+    			break;
+    		case 1:
+    			root.children.get(i).setNodeEvaluation(evaluateT( currentPos, root.children.get(i).getNodeMove()[2], opponentCurrentPos + 1, 1));
+    			break;
+    		case 2:
+    			root.children.get(i).setNodeEvaluation(evaluateT( currentPos, root.children.get(i).getNodeMove()[2], opponentCurrentPos - board.getN(), 1));
+    			break;
+    		case 3:
+    			root.children.get(i).setNodeEvaluation(evaluateT( currentPos, root.children.get(i).getNodeMove()[2], opponentCurrentPos - 1, 1));
+    			break;
+    		}
+    		
+    		if( root.children.get(i).getNodeEvaluation() > bestValue) {
+    			bestValue = root.children.get(i).getNodeEvaluation();
+    			bestMove= root.children.get(i).getNodeMove()[2];
+    		}
+    	}
+    	return bestMove;
 	}
+    
+    public int[] getNextMove (int currentPos, int opponentCurrentPos) {
+    	Node root = new Node();
+    	createMySubTree( currentPos, opponentCurrentPos, root, 1);
+    	chooseMinMaxMove( root, currentPos, opponentCurrentPos);
+    }
 
+    
 	public void createMySubTree(int currentPos, int opponentCurrentPos, Node root, int depth) {
-		Node up = new Node(root, board, x, y + 1, 0);
-		up.setNodeEvaluation(evaluateT(currentPos, 0, opponentCurrentPos, 1));
+		
+		double temp = evaluateT(currentPos, 0, opponentCurrentPos, 1);
+		if( temp != 420.0) {
+			Node up = new Node(root, board, x, y + 1, 0);
+			//up.setNodeEvaluation(temp);
+			root.children.add(up);
+			createOpponentSubtree( currentPos, opponentCurrentPos, up, depth+1, temp);
+		}
 
-		Node right = new Node(root, board, x + 1, y, 1);
-		up.setNodeEvaluation(evaluateT(currentPos, 1, opponentCurrentPos, 1));
+		temp = evaluateT(currentPos, 1, opponentCurrentPos, 1);
+		if( temp != 420.0) {
+			Node right = new Node(root, board, x + 1, y, 1);
+			//right.setNodeEvaluation(temp);
+			root.children.add(right);
+			createOpponentSubtree( currentPos, opponentCurrentPos, right, depth+1, temp);
+		}
 
-		Node down = new Node(root, board, x, y - 1, 2);
-		up.setNodeEvaluation(evaluateT(currentPos, 2, opponentCurrentPos, 1));
-
-		Node left = new Node(root, board, x - 1, y, 3);
-		up.setNodeEvaluation(evaluateT(currentPos, 3, opponentCurrentPos, 1));
-
-		root.children.add(up);
-		root.children.add(right);
-		root.children.add(down);
-		root.children.add(left);
+		temp = evaluateT(currentPos, 2, opponentCurrentPos, 1);
+		if( temp != 420.0) {
+			Node down = new Node(root, board, x, y - 1, 2);
+			//down.setNodeEvaluation(temp);
+			root.children.add(down);
+			createOpponentSubtree( currentPos, opponentCurrentPos, down, depth+1, temp);
+		}
+		
+		temp = evaluateT(currentPos, 3, opponentCurrentPos, 1);
+		if( temp != 420.0) {
+			Node left = new Node(root, board, x - 1, y, 3);
+			//left.setNodeEvaluation(temp);
+			root.children.add(left);
+			createOpponentSubtree( currentPos, opponentCurrentPos, left, depth+1, temp);
+		}
+		
+	}
+	
+	public void createOpponentSubtree(int currentPos, int opponentCurrentPos, Node parent, int depth, double parentEval) {
+		
+		double temp;
+		switch( parent.getNodeMove()[2]) {
+		case 0:
+			temp = evaluateM(opponentCurrentPos, 0, currentPos + board.getN(), 1);
+			if( temp != 420.0) {
+				Node up = new Node(parent, board, x, y + 1, 0);
+				up.setNodeEvaluation(temp);
+				parent.children.add(up);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 1, currentPos + board.getN(), 1);
+			if( temp != 420.0) {
+				Node right = new Node(parent, board, x + 1, y, 1);
+				right.setNodeEvaluation( temp);
+				parent.children.add(right);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 2, currentPos + board.getN(), 1);
+			if( temp != 420.0) {
+				Node down = new Node(parent, board, x, y - 1, 2);
+				down.setNodeEvaluation(temp);
+				parent.children.add(down);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 3, currentPos + board.getN(), 1);
+			if( temp != 420.0) {
+				Node left = new Node(parent, board, x - 1, y, 3);
+				left.setNodeEvaluation(temp);
+				parent.children.add(left);
+			}
+			
+			break;
+		case 1:
+			temp = evaluateM(opponentCurrentPos, 0, currentPos + 1, 1);
+			if( temp != 420.0) {
+				Node up = new Node(parent, board, x, y + 1, 0);
+				up.setNodeEvaluation(temp);
+				parent.children.add(up);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 1, currentPos + 1, 1);
+			if( temp != 420.0) {
+				Node right = new Node(parent, board, x + 1, y, 1);
+				right.setNodeEvaluation( temp);
+				parent.children.add(right);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 2, currentPos + 1, 1);
+			if( temp != 420.0) {
+				Node down = new Node(parent, board, x, y - 1, 2);
+				down.setNodeEvaluation(temp);
+				parent.children.add(down);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 3, currentPos + 1, 1);
+			if( temp != 420.0) {
+				Node left = new Node(parent, board, x - 1, y, 3);
+				left.setNodeEvaluation(temp);
+				parent.children.add(left);
+			}
+			
+			break;
+		case 2:
+			temp = evaluateM(opponentCurrentPos, 0, currentPos - board.getN(), 1);
+			if( temp != 420.0) {
+				Node up = new Node(parent, board, x, y + 1, 0);
+				up.setNodeEvaluation(temp);
+				parent.children.add(up);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 1, currentPos - board.getN(), 1);
+			if( temp != 420.0) {
+				Node right = new Node(parent, board, x + 1, y, 1);
+				right.setNodeEvaluation( temp);
+				parent.children.add(right);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 2, currentPos - board.getN(), 1);
+			if( temp != 420.0) {
+				Node down = new Node(parent, board, x, y - 1, 2);
+				down.setNodeEvaluation(temp);
+				parent.children.add(down);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 3, currentPos - board.getN(), 1);
+			if( temp != 420.0) {
+				Node left = new Node(parent, board, x - 1, y, 3);
+				left.setNodeEvaluation(temp);
+				parent.children.add(left);
+			}
+			
+			break;
+		case 3:
+			temp = evaluateM(opponentCurrentPos, 0, currentPos - 1, 1);
+			if( temp != 420.0) {
+				Node up = new Node(parent, board, x, y + 1, 0);
+				up.setNodeEvaluation(temp);
+				parent.children.add(up);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 1, currentPos - 1, 1);
+			if( temp != 420.0) {
+				Node right = new Node(parent, board, x + 1, y, 1);
+				right.setNodeEvaluation( temp);
+				parent.children.add(right);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 2, currentPos - 1, 1);
+			if( temp != 420.0) {
+				Node down = new Node(parent, board, x, y - 1, 2);
+				down.setNodeEvaluation(temp);
+				parent.children.add(down);
+			}
+			
+			temp = evaluateM(opponentCurrentPos, 3, currentPos - 1, 1);
+			if( temp != 420.0) {
+				Node left = new Node(parent, board, x - 1, y, 3);
+				left.setNodeEvaluation(temp);
+				parent.children.add(left);
+			}
+			
+			break;
+		}
 	}
 }
